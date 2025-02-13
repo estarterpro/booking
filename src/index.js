bd77dc6e906dbaa1a6827f1b88c86ab8e480df12
propiter <pro.pedro0413@gmail.com>
2025-02-13 17:46:43 -0500

    sync bold.diy

--- src/index.js
+++ src/index.js
@@ -3,20 +3,17 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter } from "react-router-dom";
-import AppRouter from "./AppRouter";
+import App from "./App";
 import theme from "./theme/theme";
-import { AuthProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <AppRouter />
-        </ThemeProvider>
-      </AuthProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
